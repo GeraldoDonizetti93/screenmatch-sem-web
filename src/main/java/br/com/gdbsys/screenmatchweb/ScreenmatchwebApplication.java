5b4d9e8b54a4b72080f6aab6fa7b2951254783cd
Zetti93 <geraldo.donizetti93@hotmail.com>
2024-10-25 04:45:22 -0300

    Criação do record DadosEpisodio, assim tratando as informações relacionadas ao EP.

--- src/main/java/br/com/gdbsys/screenmatchweb/ScreenmatchwebApplication.java
+++ src/main/java/br/com/gdbsys/screenmatchweb/ScreenmatchwebApplication.java
@@ -1,5 +1,6 @@
 package br.com.gdbsys.screenmatchweb;
 
+import br.com.gdbsys.screenmatchweb.model.DadosEpisodio;
 import br.com.gdbsys.screenmatchweb.model.DadosSerie;
 import br.com.gdbsys.screenmatchweb.service.ConsumoAPI;
 import br.com.gdbsys.screenmatchweb.service.ConverteDados;
@@ -17,14 +18,23 @@ public class ScreenmatchwebApplication implements CommandLineRunner {
   @Override
   public void run(String... args) throws Exception {
     var consumoAPI = new ConsumoAPI();
-    var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
+    var jsonSerie =
+        consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
 
-    System.out.println(json);
+    System.out.println(jsonSerie);
 
     var conversor = new ConverteDados();
-    var dados = conversor.obterDados(json, DadosSerie.class);
+    var dadosSerie = conversor.obterDados(jsonSerie, DadosSerie.class);
 
-    System.out.println(dados);
+    System.out.println(dadosSerie);
+
+    var jsonEpisodio =
+        consumoAPI.obterDados(
+            "https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=6585022c");
+    System.out.println(jsonEpisodio);
+
+    var dadosEpisodio = conversor.obterDados(jsonEpisodio, DadosEpisodio.class);
+    System.out.println(dadosEpisodio);
 
     //    json = consumoAPI.obterDados("https://coffee.alexflipnote.dev/random.json");
     //    System.out.println(json);
